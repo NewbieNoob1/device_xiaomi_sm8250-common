luka177/android_device_xiaomi_sm8250-common

sm8250-common: Exclude unneeded targets from tablet builds
Change-Id: I921f54a6eec98a390783d45cf9bf228593fe5e78

sm8250-common: make radio props specific to phones

Change-Id: Ic2a02b127f4f3ad86c6424edfb72fa5ac9670bfa

BoardConfigCommon.mk
@@ -166,6 +166,9 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+ifneq ($(PRODUCT_IS_TABLET),true)
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor_phone.prop
+endif
 
 # Recovery
 ifeq ($(TARGET_IS_VAB),true)
@@ -232,6 +235,10 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
+
+ifneq ($(PRODUCT_IS_TABLET),true)
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_phone.xml
+endif
 
 # Wi-Fi
 BOARD_WLAN_DEVICE := qcwcn

NewbieNoob1/device_xiaomi_sm8250-common

BoardConfigCommon.mk
@@ -165,6 +165,9 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+ifneq ($(TARGET_IS_TABLET),true)
+TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor_phone.prop
+endif
 
 # Recovery
 ifeq ($(TARGET_IS_VAB),true)
@@ -228,6 +231,9 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+ifneq ($(TARGET_IS_TABLET),true)
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest_phone.xml
+endif
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml